955d4035922e3f169d4395cbcfef9d894887042a
Jorge Mancero <jman0829@gmail.com>
2019-10-27 17:58:52 -0400

    html and js updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,7 +28,18 @@ renderButtons();
 
 
 
-
+$(".gif").on("click", function() {
+   
+    var state = $(this).attr("data-state");
+   
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
 
 // for (var i = 0; i < animals.length; i++) {
